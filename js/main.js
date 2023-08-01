2f35f868baf7e7d377239861c7162f326995d11a
fojogrimmo <afanasyvekarindm@gmail.com>
2023-08-01 18:15:43 +0300

    Finish code rewriting

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 "use strict";
 
 import SelectTab from "./select-tab.js";
-import PercentageCalculator from "./percentage.js";
-import FactorialCalculator from "./factorial.js";
 new SelectTab();
 
 class Calculator {
@@ -46,6 +44,112 @@ class Calculator {
       case "n!":
         this.display.innerText += "!";
         break;
+      case "×":
+      case "÷":
+      case "+":
+      case "-":
+        if (!this.lastInputIsOperator) {
+          this.display.innerText += e.target.innerText;
+          this.lastInputIsOperator = true;
+        } else {
+          this.display.innerText =
+            this.display.innerText.slice(0, -1) + e.target.innerText;
+        }
+        break;
+      case "()":
+        const openingParenthesis = "(";
+        const closingParenthesis = ")";
+
+        if (this.parenthesesCounter % 2 === 0) {
+          this.display.innerText += openingParenthesis;
+          this.parenthesesCounter++;
+        } else {
+          this.display.innerText += closingParenthesis;
+          this.parenthesesCounter--;
+        }
+        break;
+      case ".":
+        const currentNumber = this.display.innerText.split(/[+\-*/]/).pop();
+        if (!currentNumber.includes(".")) {
+          this.display.innerText += ".";
+        }
+        break;
+      case "Exp(x)":
+        this.display.innerText += "Math.exp(";
+        this.parenthesesCounter++;
+        break;
+      case "Ln(x)":
+        this.display.innerText += "Math.log(";
+        this.parenthesesCounter++;
+        break;
+      case "Log(x)":
+        this.display.innerText += "Math.log10(";
+        this.parenthesesCounter++;
+        break;
+      case "|x|":
+        this.display.innerText += "Math.abs(";
+        this.parenthesesCounter++;
+        break;
+
+      case "Sin(x)":
+        this.display.innerText += "Math.sin(";
+        this.parenthesesCounter++;
+        break;
+      case "Cos(x)":
+        this.display.innerText += "Math.cos(";
+        this.parenthesesCounter++;
+        break;
+      case "Tan(x)":
+        this.display.innerText += "Math.tan(";
+        this.parenthesesCounter++;
+        break;
+      case "Cot(x)":
+        this.display.innerText += "1 / Math.tan(";
+        this.parenthesesCounter++;
+        break;
+
+      case "Asin(x)":
+        this.display.innerText += "Math.asin(";
+        this.parenthesesCounter++;
+        break;
+      case "Acos(x)":
+        this.display.innerText += "Math.acos(";
+        this.parenthesesCounter++;
+        break;
+      case "Atan(x)":
+        this.display.innerText += "Math.atan(";
+        this.parenthesesCounter++;
+        break;
+      case "e":
+        const eNumber = Math.E;
+        this.display.innerText += eNumber.toFixed(5);
+        break;
+      case "√(x)":
+        this.display.innerText += "Math.sqrt(";
+        this.parenthesesCounter++;
+        break;
+      case "x2":
+        this.display.innerText += "^2";
+        break;
+
+      case "1/x":
+        this.display.innerText += "1 / ";
+        this.parenthesesCounter++;
+        break;
+      case "π":
+        const piValue = Math.PI;
+        this.display.innerText += piValue.toFixed(5);
+        break;
+      case "n!":
+        this.display.innerText += "!";
+        break;
+      case "xy":
+        this.display.innerText += "^";
+        break;
+      case "10x":
+        this.display.innerText += "10^";
+        break;
+
       default:
         this.display.innerText += e.target.innerText;
         this.lastInputIsOperator = false;
@@ -80,196 +184,53 @@ class Calculator {
     }
   }
 }
+
 new Calculator();
-// const display = document.querySelector(".output");
-// let buttons = [...document.querySelectorAll("[data-button]")];
-// let parenthesesCounter = 0;
-// let lastInputIsOperator = false;
 
-// function calculatePercentage(number, percentage) {
-//   return (number * percentage) / 100;
-// }
+class FactorialCalculator {
+  static findFactorial(num) {
+    if (num === 0 || num === 1) return 1;
+    else {
+      return (num = num * FactorialCalculator.findFactorial(num - 1));
+    }
+  }
+}
 
-// function isOperator(value) {
-//   return ["+", "-", "×", "÷"].includes(value);
-// }
+new FactorialCalculator();
 
-// buttons.map((button) => {
-//   button.addEventListener("click", (e) => {
-//     if (display.innerText === "Error") {
-//       display.innerText = "";
-//       parenthesesCounter = 0;
-//     }
+class PercentageCalculator {
+  constructor(display) {
+    this.display = display;
+  }
 
-//     switch (e.target.innerText) {
-//       case "C":
-//         display.innerText = "";
-//         parenthesesCounter = 0;
-//         break;
-//       case "DEL":
-//         if (display.innerText) {
-//           display.innerText = display.innerText.slice(0, -1);
-//         }
-//         break;
-//       case "=":
-//         let expression = display.innerText
-//           .replace(/×/g, "*")
-//           .replace(/÷/g, "/")
-//           .replace(/\^/g, "**")
-//           .replace(/(\d+)!/g, "findFactorial($1)");
+  calculatePercentage() {
+    const expression = this.display.innerText.split(/[-+×÷]/);
+    const lastNumber = expression[expression.length - 1];
+    const percentage = parseFloat(lastNumber);
+    const previousNumber = parseFloat(expression[0]);
+    const operand = this.display.innerText.match(/[+-]/);
+    let result;
 
-//         try {
-//           const result = eval(expression);
-//           if (isNaN(result) || !isFinite(result)) {
-//             display.innerText = "Error";
-//           } else {
-//             display.innerText = result;
-//           }
-//         } catch (error) {
-//           if (error instanceof SyntaxError) {
-//             display.innerText = "Error";
-//           } else {
-//             throw error;
-//           }
-//         }
-//         break;
-//   case "%":
-//     const expressionParts = display.innerText.split(/[-+×÷]/);
-//     const lastNumber = expressionParts[expressionParts.length - 1];
-//     const percentage = parseFloat(lastNumber);
-//     const previousNumber = parseFloat(expressionParts[0]);
-//     const operand = display.innerText.match(/[+-]/);
-//     let result;
+    if (expression.length < 2) {
+      result = previousNumber;
+    } else if (operand && operand[0] === "+") {
+      result =
+        previousNumber +
+        this.calculatePercentageValue(previousNumber, percentage);
+    } else if (operand && operand[0] === "-") {
+      result =
+        previousNumber -
+        this.calculatePercentageValue(previousNumber, percentage);
+    } else {
+      result = this.calculatePercentageValue(previousNumber, percentage);
+    }
 
-//     if (expressionParts.length < 2) {
-//       result = previousNumber;
-//     } else if (operand && operand[0] === "+") {
-//       result =
-//         previousNumber + calculatePercentage(previousNumber, percentage);
-//     } else if (operand && operand[0] === "-") {
-//       result =
-//         previousNumber - calculatePercentage(previousNumber, percentage);
-//     } else {
-//       result = calculatePercentage(previousNumber, percentage);
-//     }
+    this.display.innerText = result;
+  }
 
-//     display.innerText = result;
-//     break;
-//       case "×":
-//       case "÷":
-//       case "+":
-//       case "-":
-//         if (!lastInputIsOperator) {
-//           display.innerText += e.target.innerText;
-//           lastInputIsOperator = true;
-//         } else {
-//           display.innerText =
-//             display.innerText.slice(0, -1) + e.target.innerText;
-//         }
-//         break;
-//       case "×":
-//         display.innerText += "*";
-//         break;
-//       case "÷":
-//         display.innerText += "/";
-//         break;
-//       case "()":
-//         const openingParenthesis = "(";
-//         const closingParenthesis = ")";
+  calculatePercentageValue(base, percentage) {
+    return (base * percentage) / 100;
+  }
+}
 
-//         if (parenthesesCounter % 2 === 0) {
-//           display.innerText += openingParenthesis;
-//           parenthesesCounter++;
-//         } else {
-//           display.innerText += closingParenthesis;
-//           parenthesesCounter--;
-//         }
-//         break;
-//       case ".":
-//         const currentNumber = display.innerText.split(/[+\-*/]/).pop();
-//         if (!currentNumber.includes(".")) {
-//           display.innerText += ".";
-//         }
-//         break;
-//       case "Exp(x)":
-//         display.innerText += "Math.exp(";
-//         parenthesesCounter++;
-//         break;
-//       case "Ln(x)":
-//         display.innerText += "Math.log(";
-//         parenthesesCounter++;
-//         break;
-//       case "Log(x)":
-//         display.innerText += "Math.log10(";
-//         parenthesesCounter++;
-//         break;
-//       case "|x|":
-//         display.innerText += "Math.abs(";
-//         parenthesesCounter++;
-//         break;
-
-//       case "Sin(x)":
-//         display.innerText += "Math.sin(";
-//         parenthesesCounter++;
-//         break;
-//       case "Cos(x)":
-//         display.innerText += "Math.cos(";
-//         parenthesesCounter++;
-//         break;
-//       case "Tan(x)":
-//         display.innerText += "Math.tan(";
-//         parenthesesCounter++;
-//         break;
-//       case "Cot(x)":
-//         display.innerText += "1 / Math.tan(";
-//         parenthesesCounter++;
-//         break;
-
-//       case "Asin(x)":
-//         display.innerText += "Math.asin(";
-//         parenthesesCounter++;
-//         break;
-//       case "Acos(x)":
-//         display.innerText += "Math.acos(";
-//         parenthesesCounter++;
-//         break;
-//       case "Atan(x)":
-//         display.innerText += "Math.atan(";
-//         parenthesesCounter++;
-//         break;
-//       case "e":
-//         const eNumber = Math.E;
-//         display.innerText += eNumber.toFixed(5);
-//         break;
-//       case "√(x)":
-//         display.innerText += "Math.sqrt(";
-//         parenthesesCounter++;
-//         break;
-//       case "x2":
-//         display.innerText += "^2";
-//         break;
-
-//       case "1/x":
-//         display.innerText += "1 / ";
-//         parenthesesCounter++;
-//         break;
-//       case "π":
-//         const piValue = Math.PI;
-//         display.innerText += piValue.toFixed(5);
-//         break;
-//       case "n!":
-//         display.innerText += "!";
-//         break;
-//       case "xy":
-//         display.innerText += "^";
-//         break;
-//       case "10x":
-//         display.innerText += "10^";
-//         break;
-//       default:
-//         display.innerText += e.target.innerText;
-//         lastInputIsOperator = false;
-//         break;
-//     }
-//   });
-// });
+new PercentageCalculator();
